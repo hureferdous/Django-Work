ab335a2939c3cbe56cfdc9a4153b6a9db685d149
Hure Ferdous <hure_fer@yahoo.com>
2017-07-24 16:31:48 +0100

    Create New Project and Coding for Template Inharitance

---
+++ MenuTest/sitepages/views.py
@@ -0,0 +1,25 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+
+def home_page(request):
+    return render(request,"landing_page/home.html",
+                  {"home": "Home",
+                   "home_p": "Fantastic Portfolio Website Designs for Inspiration. Selection of Awwwards winning portfolio websites."})
+
+
+def contact_page(request):
+    return render(request, "landing_page/contact.html",
+                  { "contact":"Contact",
+                    "contact_p":"40 Locks Yard, Headcorn, Ashford, Kent, UK ,Tn279AD"
+                  })
+
+def about_page(request):
+    return render(request,"landing_page/about.html",
+                  { "about":"About",
+                    "about_p":"Following the latest update of portfolio builder Cargo, we round up the 10 best portfolio websites artists designers to showcase their work. Behance. Adobe Portfolio."
+
+                  })
